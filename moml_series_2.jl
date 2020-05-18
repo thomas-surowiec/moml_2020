05bdda08aef6b31de24268eef105cd5e215f06bb
Thomas Surowiec <thomas.surowiec@gmail.com>
2020-05-18 17:19:26 +0200

    completed all functions. needs commenting.

--- moml_series_2.jl
+++ moml_series_2.jl
@@ -21,6 +21,7 @@ function ell_lp_norm(x::Vector{Float64},p::Int64)
 end
 ell_lp_norm(randn(1),3)
 
+
 function quadratic_objective(A::Matrix{Float64},
                              b::Vector{Float64},
                              x::Vector{Float64},
@@ -36,7 +37,7 @@ function grad_f_ell_2(A::Matrix{Float64},b::Vector{Float64},x::Vector{Float64})
     temp = A'*temp
     return temp - A'*b
 end
-@time grad_f_ell_2(rand(2,2),rand(2),rand(2))
+grad_f_ell_2(rand(2,2),rand(2),rand(2))
 
 function subdiff_ell_1(x::Vector{Float64})
     q = zeros(length(x))
@@ -49,18 +50,31 @@ function subdiff_ell_1(x::Vector{Float64})
             q[i] = 2*rand()-1
         end
     end
+
     return q
 end
 subdiff_ell_1(randn(10))
 
-function nonsmooth_objective(A::Matrix{Float64},
-                             b::Vector{Float64},
-                             x::Vector{Float64})
+function ell_l1_norm_nsq(A::Matrix{Float64},
+                         b::Vector{Float64},
+                         x::Vector{Float64})
     z = A*x - b
     q = subdiff_ell_1(z)
+
     return A'*q
 end
-nonsmooth_objective(rand(20,20),rand(20),randn(20))
+ell_l1_norm_nsq(rand(20,20),rand(20),randn(20))
+
+function ell_l1_norm_sq(A::Matrix{Float64},
+                        b::Vector{Float64},
+                        x::Vector{Float64})
+    z = A*x - b
+    q = subdiff_ell_1(z)
+
+    return ell_lp_norm(z,1)*A'*q
+end
+ell_l1_norm_sq(rand(20,20),rand(20),randn(20))
+
 
 function projection_Rn_plus(x::Vector{Float64})
     proj = zeros(length(x))
@@ -95,3 +109,13 @@ end
 a = -1*ones(10)
 b = ones(10)
 projection_ab(randn(10),a,b)
+
+function projection_hp(x::Vector{Float64},w::Vector{Float64},b::Float64)
+    c_1 = w'*x + b
+    c_2 = ell_lp_norm(w,2)
+    return x - (c_1/c_2)*w
+end
+w  = rand(3)
+bb = 1.0
+xx = randn(3)
+projection_hp_1(xx,w,bb)
