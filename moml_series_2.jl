53fe7a458b0af9c0f59b6761edd3b9b9ab1d49c6
Thomas Surowiec <thomas.surowiec@gmail.com>
2020-05-18 15:10:44 +0200

    part I added.

---
+++ moml_series_2.jl
@@ -0,0 +1,22 @@
+# MOML Series 2 Problem 3
+function ell_lp_norm(x::Vector{Float64},p::Int64)
+    # p = 1,2
+    if p == 1
+        elp = 0.0
+        for i in 1:length(x)
+            elp = elp + abs(x[i])
+        end
+    elseif p == 2
+        elp = 0.0
+        for i in 1:length(x)
+            elp = elp + x[i]^2
+        end
+    else
+        elp = 0.0
+        for i in 1:length(x)
+            elp = max(elp,abs(x[i]))
+        end
+    end
+    return elp
+end
+@time ell_lp_norm(randn(100000),2)
