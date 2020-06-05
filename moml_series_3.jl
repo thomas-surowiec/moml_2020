2db95c370a009cddccc2ee6460ff10d4ab962895
Thomas Surowiec <surowiec@mathematik.uni-marburg.de>
2020-06-05 15:26:51 +0200

    working on objective structs

--- moml_series_3.jl
+++ moml_series_3.jl
@@ -29,3 +29,46 @@ end
 
 P_X = Projection(projection_ab)
 P_X.Proj(randn(10),-1*ones(10),ones(10))
+
+
+function ell_lp_norm(x::Vector{Float64},p::Int64)
+    # p = 1,2
+    if p == 1
+        elp = 0.0
+        for i in 1:length(x)
+            elp = elp + abs(x[i])
+        end
+    elseif p == 2
+        elp = 0.0
+        for i in 1:length(x)
+            elp = elp + x[i]^2
+        end
+    else
+        elp = 0.0
+        for i in 1:length(x)
+            elp = max(elp,abs(x[i]))
+        end
+    end
+    return elp
+end
+
+
+function quadratic_objective(A::Matrix{Float64},
+                             b::Vector{Float64},
+                             x::Vector{Float64},
+                             p::Int64)
+    z = A*x - b
+    f = ell_lp_norm(z,p)
+    return 0.5*f^2
+end
+
+
+function grad_f_ell_2(A::Matrix{Float64},b::Vector{Float64},x::Vector{Float64})
+    temp = A*x
+    temp = A'*temp
+    return temp - A'*b
+end
+
+
+f = Objective(quadratic_objective,grad_f_ell_2)
+f.Obj(rand(2,2),rand(2),rand(2),1)
